c88ff48ca91fd7b0bb86b43f15ca8c55c6e301c4
chalireJ107 <charlie_j107@outlook.com>
2023-12-21 20:54:26 +0000

    Add data interfaces

--- src/models/paper.ts
+++ src/models/paper.ts
@@ -1,6 +1,35 @@
+import { Author } from "./author";
+import { Group } from "./group";
+import { Tag } from "./tag";
+
 export interface Paper {
     title: string;
-    date: Date;
+    authors: Author[];
+    publication: string;
+    publishTime: Date;
+    publishType: PublishType;
     description: string;
-    authors: string[];
+    urls: string[];
+    addTime: Date;
+    codes: string[];
+    pages: string;
+    volume: string;
+    number: string;
+    publisher: string;
+    rating: number;
+    tags: Tag;
+    flagged: boolean;
+    group: Group;
+    doi?: string;
+    arxivId?: string;
+
+    [key: string]: unknown; // for future extension
+
+}
+
+enum PublishType {
+    Journal = "Journal",
+    Conference = "Conference",
+    Workshop = "Workshop",
+    Other = "Other"
 }
