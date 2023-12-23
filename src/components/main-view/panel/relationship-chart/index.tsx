3432008a279e0d2c78440d74596e44ee3d2e4dc3
charlieJ107 <charlie_j107@outlook.com>
2023-12-23 10:45:34 +0000

    Add relationship chart with paper data

--- src/components/main-view/panel/relationship-chart/index.tsx
+++ src/components/main-view/panel/relationship-chart/index.tsx
@@ -1,11 +1,34 @@
 import * as echarts from 'echarts/core';
 import { useRef } from 'react';
+import { useTheme } from '../../../../contexts/theme/theme-context';
+import { EChartsOption } from 'echarts';
+import { usePaperContext } from '../../../../contexts/papers/paper-context';
 
-export default function RelationshipChart(){
+export default function RelationshipChart() {
     const chartRef = useRef<HTMLDivElement>(null);
-    if (chartRef.current !== null) {
-        chart = init(chartRef.current, theme);
-      }
+    const theme = useTheme();
+    const papers = usePaperContext();
+    const paperData = papers.filteredPapers.map(paper => ({
+        name: paper.title,
+        id: paper.id,
+        symbolSize: 10
+    }));
+    const chart = echarts.init(chartRef.current, theme);
+    chart.setOption<EChartsOption>({
+        serise: [
+            {
+                type: 'graph',
+                layout: 'force',
+                data: paperData,
+                force: {
+                    repulsion: 50,
+                    edgeLength: 10
+                },
+            }
+        ]
+
+    });
+
     return (
         <div ref={chartRef}>
 
