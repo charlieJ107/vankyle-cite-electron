893ea7dd3d900a40ee8623f047c6a76f9df9e322
charlieJ107 <charlie_j107@outlook.com>
2023-12-23 22:49:16 +0000

    Refactor Library component styling

--- src/components/main-view/library/index.tsx
+++ src/components/main-view/library/index.tsx
@@ -1,10 +1,10 @@
-import { mergeClasses, typographyStyles } from "@fluentui/react-components";
+import { mergeClasses } from "@fluentui/react-components";
 import PapersList from "./papers";
 
 
 export default function Library({ className }: { className?: string }) {
     return (
-        <section className={mergeClasses(className, "flex flex-col flex-grow rounded m-3")}>
+        <section className={mergeClasses(className, "flex flex-col flex-grow rounded m-3 mx-0")}>
             <PapersList />
         </section>
     );
