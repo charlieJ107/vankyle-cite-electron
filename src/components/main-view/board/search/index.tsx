f2946518fe02c05c41f2a352fccd6d8699d56d64
chalireJ107 <charlie_j107@outlook.com>
2023-12-25 22:41:51 +0000

    Delete search and tags components

--- src/components/main-view/board/search/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Textarea } from "@fluentui/react-components";
-
-export default function Search() {
-    return (
-        <div className="flex flex-grow flex-col m-3 max-w-80">
-            <Textarea className="" placeholder="Ask AI about your papers" />
-        </div>
-    );
-}
