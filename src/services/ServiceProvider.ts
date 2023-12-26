97384ad61619c7b8597229fa01c3dec76562b4d3
chalireJ107 <charlie_j107@outlook.com>
2023-12-26 15:34:52 +0000

    Add new files and interfaces for app configuration and services

---
+++ src/services/ServiceProvider.ts
@@ -0,0 +1,36 @@
+export class ServiceProvider {
+    constructor() {
+
+    }
+    private dependencies: { [key: string]: any } = {};
+
+    registerService<T>(name: string, dependency: T) {
+        if (!this.dependencies[name]) {
+            this.dependencies[name] = dependency;
+        }
+    }
+
+    resolveService<T>(target: new (...args: any[]) => T): T {
+        const paramNames = this.getParamNames(target);
+
+        const dependencies = paramNames.map((name: string) => {
+            if (!this.dependencies[name]) {
+                throw new Error(`Dependency '${name}' not found.`);
+            }
+            return this.dependencies[name];
+        });
+
+        return new target(...dependencies);
+    }
+    private getParamNames(func: Function): string[] {
+        const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+        const ARGUMENT_NAMES = /([^\s,]+)/g;
+
+        const fnStr = func.toString().replace(STRIP_COMMENTS, '');
+        const result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+        if (result === null) {
+            return [];
+        }
+        return result;
+    }
+}
