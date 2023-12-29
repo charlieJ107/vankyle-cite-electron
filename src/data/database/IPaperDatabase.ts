ca81a890279ce6e506eba43cf530e4b826bc272e
chalireJ107 <charlie_j107@outlook.com>
2023-12-29 03:28:49 +0000

    Refactor paper database implementation

--- src/data/database/IPaperDatabase.ts
+++
@@ -1,8 +0,0 @@
-import { Paper } from "../../models/paper";
-
-export interface IPaperDatabase {
-    getPaper(id: string): Promise<Paper | null>;
-    getPaperList(filter?: (paper: Paper) => boolean): Promise<Paper[]>;
-    savePaper(paper: Paper): Promise<void>;
-    deletePaper(id: string): Promise<void>;
-}
--- src/data/database/local/JsonFilePaperDatabase.ts
+++ src/data/database/local/JsonFilePaperDatabase.ts
@@ -1,15 +1,17 @@
 import { Paper } from "../../../models/paper";
-import { IPaperDatabase } from "../IPaperDatabase";
+import { IDatabase } from "../IDatabase";
 import fs from "fs";
+import path from "path";
 
-export class JsonFilePaperDatabase implements IPaperDatabase {
-    constructor() {
+export class JsonFilePaperDatabase implements IDatabase<Paper> {
+    private dataDir: string;
+    constructor(dataDir: string) {
+        this.dataDir = dataDir;
     }
-
     private buffer: { [key: string]: Paper; } = {};
 
     private async updateBuffer() {
-        return await fs.readFile("data.json", "utf8", (err: NodeJS.ErrnoException | null, jsonString: string) => {
+        return await fs.readFile(path.join(this.dataDir, "data.json"), "utf8", (err: NodeJS.ErrnoException | null, jsonString: string) => {
             if (err) {
                 console.error("File read failed:", err);
                 return;
@@ -27,20 +29,20 @@ export class JsonFilePaperDatabase implements IPaperDatabase {
         });
     }
 
-    async getPaper(id: string): Promise<Paper | null> {
+    async get(id: string): Promise<Paper | null> {
         this.updateBuffer();
         return this.buffer[id];
     }
-    async getPaperList(): Promise<Paper[]> {
+    async getList(filter?: ((model: Paper) => boolean) | undefined): Promise<Paper[]> {
         this.updateBuffer();
-        return Promise.resolve(Object.values(this.buffer));
+        return Promise.resolve(Object.values(this.buffer).filter(filter || (() => true)));
     }
-    async savePaper(paper: Paper): Promise<void> {
+    async save(paper: Paper): Promise<void> {
         this.buffer[paper._id] = paper;
         return await this.saveBuffer();
-        
+
     }
-    async  deletePaper(id: string): Promise<void> {
+    async delete(id: string): Promise<void> {
         delete this.buffer[id];
         return this.saveBuffer();
     }
