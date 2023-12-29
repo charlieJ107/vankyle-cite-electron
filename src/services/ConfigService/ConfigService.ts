e49f10fe01c88f737dc3c402c00df46416dfb6c8
chalireJ107 <charlie_j107@outlook.com>
2023-12-29 15:30:30 +0000

    Add default configuration and file handling in ConfigService

--- src/services/ConfigService/ConfigService.ts
+++ src/services/ConfigService/ConfigService.ts
@@ -1,9 +1,32 @@
+import { IConfig } from "../../common/config/ConfigInterfaces";
 import { IService } from "../IService";
+import fs from "fs";
+import path from "path";
+
+const defaultConfig: IConfig = {
+    plugins: {
+        plugin_dir: "plugins",
+        enabled_plugins: []
+    }
+}
 
 export class ConfigService implements IService {
-    constructor() {
+    private rootPath: string;
+    private config: IConfig;
+    constructor(rootPath: string) {
+        this.rootPath = rootPath;
+        this.config = defaultConfig;
+        if (!(fs.existsSync(rootPath))) {
+            fs.mkdirSync(rootPath);
+        }
+        if (!(fs.existsSync(path.join(this.rootPath, "config.json")))) {
+            fs.writeFileSync(path.join(this.rootPath, "config.json"), JSON.stringify(this.config));
+        } else {
+            this.config = JSON.parse(fs.readFileSync(path.join(this.rootPath, "config.json")).toString()) as IConfig;
+        }
     }
 
-    public init(): void {
+    public getConfig(): IConfig {
+        return this.config;
     }
 }
