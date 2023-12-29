d1212b3d6755d024a4094fe2ddac9d5636f2f39c
chalireJ107 <charlie_j107@outlook.com>
2023-12-29 15:29:42 +0000

    Refactor LocalFileSystemStorage class

--- src/data/storage/local/FileSystemPaperStorage.ts
+++ src/data/storage/local/FileSystemPaperStorage.ts
@@ -10,23 +10,18 @@ export class LocalFileSystemStorage implements IFileStorage {
         this.rootPath = rootPath;
     }
     getPath(id: string): Promise<string> {
-        // get file from rootPath, file name is id
         return Promise.resolve(this.rootPath + "/" + id);
     }
     get(id: string): Promise<Buffer> {
-        // get file from rootPath, file name is id
         return Promise.resolve(fs.readFileSync(this.rootPath + "/" + id));
     }
     save(id: string, file: Buffer): Promise<void> {
-        // save file to rootPath, file name is id
         return Promise.resolve(fs.writeFileSync(this.rootPath + "/" + id, file));
     }
     saveByPath(id: string, path: string): Promise<void> {
-        // save file to rootPath, file name is id
         return Promise.resolve(fs.copyFileSync(path, this.rootPath + "/" + id));
     }
     delete(id: string): Promise<void> {
-        // delete file from rootPath, file name is id
         return Promise.resolve(fs.unlinkSync(this.rootPath + "/" + id));
     }
 
