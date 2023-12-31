e7308575b159f2a8e24c61115aa629b3df7fc4a6
chalireJ107 <charlie_j107@outlook.com>
2023-12-31 05:20:22 +0000

    Update package.json and tsconfig.json, and modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,53 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron';
+import electronSimple from 'vite-plugin-electron/simple';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'src/main.ts',
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'src/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
-  ],
-})
+/** @type {import('vite').UserConfig} */
+export default {
+    plugins: [
+        react(),
+        electronSimple({
+            main: {
+                // Shortcut of `build.lib.entry`.
+                entry: 'src/main.ts',
+                vite: {
+                    build: {
+                        outDir: "dist",
+                    }
+                }
+            },
+            preload: {
+                // Shortcut of `build.rollupOptions.input`.
+                // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+                input: path.join(__dirname, 'src/preload.ts'),
+                vite: {
+                    build: {
+                        outDir: "dist",
+                    }
+                }
+            },
+            // Ployfill the Electron and Node.js built-in modules for Renderer process.
+            // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+            renderer: {},
+        }),
+        electron([
+            {
+                entry: "src/plugins/index.ts",
+                vite: {
+                    build: {
+                        outDir: "dist/plugins",
+                    }
+                }
+            },
+            {
+                entry: "src/services/index.ts",
+                vite: {
+                    build: {
+                        outDir: "dist/services",
+                    }
+                }
+            }
+        ])
+    ]
+};
