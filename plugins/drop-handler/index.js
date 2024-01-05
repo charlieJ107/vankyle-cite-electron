770be26b6512813aa374fd81af0f3e317059a2c3
chalireJ107 <charlie_j107@outlook.com>
2024-01-05 15:04:25 +0000

    Add drop-handler plugin

--- plugins/drop-handler/index.js
+++ plugins/drop-handler/index.js
@@ -1,3 +1,7 @@
 window.AppServices.PaperService.getAllPapers().then(papers => {
     console.log(papers);
 });
+
+window.AppServices.DropService.registerDropHandler("drop-handler", (dropData) => {
+        console.log(dropData);
+    });
