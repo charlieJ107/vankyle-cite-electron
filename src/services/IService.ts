866a9d8400687af4aad5b58237cd61ffff900a3e
charlieJ107 <charlie_j107@outlook.com>
2024-01-10 00:25:02 +0000

    Remove unnecessary whitespace in IService.ts

--- src/services/IService.ts
+++ src/services/IService.ts
@@ -1,9 +1,7 @@
-import { MessagePortRpcAgent } from "@/common/rpc/MessagePortRpcAgent";
 
 export function Service() {
     return function (value: new (...args: any[]) => any, context: ClassDecoratorContext) {
         const methods = Object.getOwnPropertyNames(value.prototype).filter((name) => name !== "constructor" && typeof value.prototype[name] === "function");
 
-        
     }
 }
