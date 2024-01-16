9754bb064ca1bddd2ac2d48bac518ca6c2e8ccde
chalireJ107 <charlie_j107@outlook.com>
2024-01-16 23:18:03 +0000

    Update devDependencies and build configurations

---
+++ vankyle-cite-types/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, "..", 'src'),
+            '@models': path.resolve(__dirname, "..", 'src/models'),
+            '@components': path.resolve(__dirname, "..", 'src/components'),
+        },
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    }
+
+};
