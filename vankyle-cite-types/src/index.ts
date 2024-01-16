2f0f88674d22682dcab4dff79745574dc033e898
chalireJ107 <charlie_j107@outlook.com>
2024-01-16 23:53:38 +0000

    Update package.json and index.ts files

--- vankyle-cite-types/src/index.ts
+++ vankyle-cite-types/src/index.ts
@@ -1,10 +1,21 @@
 import { PaperService } from '@/services/PaperService/PaperService';
 import { DropService } from '@/services/DropService/DropService';
+import { FileSystemService } from '@/main/services/FileSystemService';
+import { ConfigService } from '@/services/ConfigService/ConfigService';
+import { PluginManager } from '@/services/PluginManager/PluginManager';
+import { Author } from '@/models/author';
+import { Paper } from '@/models/paper';
+import { Tag } from '@/models/tag';
+import { Group } from '@/models/group';
+import { BaseDataModel } from '@/models/DataModel';
 
-export interface App {
+interface App {
     Services: {
         PaperService: PaperService;
         DropService: DropService;
+        FileSystemService: FileSystemService;
+        ConfigService: ConfigService;
+        PluginManager: PluginManager;
     }
 }
 
@@ -12,4 +23,13 @@ declare global {
     interface Window {
         App: App;
     }
+}
+
+export {
+    App,
+    Author,
+    Paper,
+    Tag,
+    Group,
+    BaseDataModel
 }
